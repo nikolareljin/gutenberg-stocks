a81f0db54419b4e78bb7f28f75c9ef9a0429bb9a
Nik Reljin <nikola.reljin@gmail.com>
2026-08-23 22:55:10 -0400

    chore: release 0.1.1

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * Plugin URI: https://github.com/nikolareljin/gutenberg-stocks/
  * Description: stock-information — is a Gutenberg plugin created via create-guten-block.
  * Author: Nikola Reljin
- * Version: 0.1.0
+ * Version: 0.1.1
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
